c4443d5b8055e234b15a6071842dfab70588f9d9
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-01 14:47:40 -0500

    Scripted auto-commit on change (2022-04-01 14:47:40) by gitwatch.sh

--- day03/example2.scm
+++ day03/example2.scm
@@ -5,3 +5,11 @@
         (and
          (number? (car lst))
          (list-of-numbers? (cdr lst))))))
+
+(define list-of-numbers2?
+  (lambda (lst)
+    (if (null? lst)
+        #t
+        (and
+         (number? (car lst))
+         (list-of-numbers2? (cdr lst))))))
