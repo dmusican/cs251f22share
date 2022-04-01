5de660b959e78497cbbba881df2f64b390b30ca1
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-01 14:59:16 -0500

    Scripted auto-commit on change (2022-04-01 14:59:16) by gitwatch.sh

--- day03/example.scm
+++ day03/example.scm
@@ -2,3 +2,8 @@
 ;;     (b c)
 ;; (remove-first 'b '(a b c b)) --->
 ;;        (a c b)
+
+(define remove-first
+  (lambda (s lst)
+    (cond ((null? lst) '()
+           (equal? s (car lst)) (cdr lst)
