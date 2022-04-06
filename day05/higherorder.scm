b61015c03482ab95787f763f14f8e5fba8a95d1f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-06 14:18:08 -0500

    Scripted auto-commit on change (2022-04-06 14:18:08) by gitwatch.sh

--- day05/higherorder.scm
+++ day05/higherorder.scm
@@ -2,4 +2,12 @@
   (lambda (x)
     (+ x 1)))
 
+;; map takes two parameters
+;; the first is a function of one parameter
+;; the second is a list
+;;
+;; returns a list of the same size as
+;; the list that was the second parameter
+;; where the first parameter is applied
+;; to every item in that list
 (map addOne '(1 2 9 3 4))
