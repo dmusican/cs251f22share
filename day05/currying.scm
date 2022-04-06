32e72d5a03841e59057a339348f99aefda6af50c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-06 13:55:52 -0500

    Scripted auto-commit on change (2022-04-06 13:55:52) by gitwatch.sh

--- day05/currying.scm
+++ day05/currying.scm
@@ -1,7 +1,11 @@
 ;; Normal example
+;; (define mult
+;;   (lambda (a b)
+;;     (* a b)))
+
+;; Currying example instead
 (define mult
-  (lambda (a b)
-    (* a b)))
-
-
-;; Currying example
+  (lambda (a)
+    ;;; another function is returned
+    (lambda (b)
+      (* a b))))
