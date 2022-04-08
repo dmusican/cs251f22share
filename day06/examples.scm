ddcb32209781ea9b21b49ce77df9747dab7dcc8c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-08 14:59:20 -0500

    Scripted auto-commit on change (2022-04-08 14:59:20) by gitwatch.sh

--- day06/examples.scm
+++ day06/examples.scm
@@ -7,8 +7,10 @@
     (id
      (+ a b))))
 
-(define fact
-  (lambda (n)
-    (if (equal? n 1)
-        1
-        (* n (fact (- n 1))))))
+;; (define fact
+;;   (lambda (n)
+;;     (if (equal? n 1)
+;;         1
+;;         (* n (fact (- n 1))))))
+
+(
