6d070a2d5b00f03bb6f491354189d0048e16d144
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-18 14:55:40 -0500

    Scripted auto-commit on change (2022-04-18 14:55:40) by gitwatch.sh

--- day10/example3.c
+++ day10/example3.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    int *numbers[3] = malloc(3*sizeof(int));
+    int *numbers = malloc(3*sizeof(int));
     int loc = 1+5;
     numbers[loc] = 10;
     printf("%i\n", numbers[loc]);
