7136ead2117eb8cd0f38b5ebb4b55d6897e2f3fd
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-18 14:52:08 -0500

    Scripted auto-commit on change (2022-04-18 14:52:08) by gitwatch.sh

--- day10/example2.c
+++ day10/example2.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 int main() {
-    int other[15];
+    /* int other[15]; */
     int numbers[3];
     /* int more[10]; */
     int loc = 1+5;
     numbers[loc] = 10;
     printf("%i\n", numbers[loc]);
-    printf("%i\n", other[3]);
+    /* printf("%i\n", other[3]); */
 }
