c51c87294dc19fc3d1faaf04fc3ca890e072c94b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-18 14:41:26 -0500

    Scripted auto-commit on change (2022-04-18 14:41:26) by gitwatch.sh

--- day10/example1.c
+++ day10/example1.c
@@ -5,7 +5,7 @@ int main() {
     int numbers[3];
     for (int i=0; i < 3; i++) {
         numbers[i] = i;
-        printf("%i\n", numbers[i]);
+        /* printf("%i\n", numbers[i]); */
     }
     printf("%i\n", *numbers);
 
