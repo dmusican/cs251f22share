001d118df5bb5afff23f6e499ebcc0658d8dea47
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-18 14:33:02 -0500

    Scripted auto-commit on change (2022-04-18 14:33:02) by gitwatch.sh

--- day10/example1.c
+++ day10/example1.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 int main() {
 
-    unsigned int seed;
-    scanf("%u", &seed);
-    srandom(seed);
-    //srandom((unsigned int)time(NULL));
-    int randNum = random()%100 + 1;
 }
