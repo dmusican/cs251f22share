efc4223bc2f7405053eabf26aad3af69c2ba6e3e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-25 14:01:55 -0500

    Scripted auto-commit on change (2022-04-25 14:01:55) by gitwatch.sh

--- day12/interpreter.py
+++ day12/interpreter.py
@@ -1,2 +1,12 @@
 import sys
 inpfile = open('myprog.greet', 'r')
+
+for line in inpfile:
+    (command, number) = line.split()
+    if int(number) < 1:
+        print("Error should be a number")
+        sys.exit(1)
+    for i in range(int(number)):
+        print(command)
+
+inpfile.close()
