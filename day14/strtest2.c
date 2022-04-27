07d3bf7f5d53e4483243e3ad9bac7c1d26f0551e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-27 14:19:48 -0500

    Scripted auto-commit on change (2022-04-27 14:19:48) by gitwatch.sh

--- day14/strtest2.c
+++ day14/strtest2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     // the easy way
-    char *word = "dave";
+    char *word = malloc(10*sizeof(char));
+    strcpy(word, "dave");
     word[0] = 's';
     printf("%s\n", word);
 }
