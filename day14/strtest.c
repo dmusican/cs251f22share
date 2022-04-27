101a264978fd831d46b0f7fe2f47001536fcc4a1
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-27 13:59:43 -0500

    Scripted auto-commit on change (2022-04-27 13:59:43) by gitwatch.sh

---
+++ day14/strtest.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // the hard way
+    char word[20];
+    word[0] = 'd';
+    word[1] = 'a';
+    word[2] = 'v';
+    word[3] = 'e';
+    word[4] = '\0';  // null
