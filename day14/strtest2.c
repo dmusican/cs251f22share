28e9e0c85077f5f9000916b0f4ee3f1c6f1dc3fc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-04-27 14:06:41 -0500

    Scripted auto-commit on change (2022-04-27 14:06:41) by gitwatch.sh

--- day14/strtest2.c
+++ day14/strtest2.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // the hard way
-    char word[10];
-    word[0] = 'd';
-    word[1] = 'a';
-    word[2] = 'v';
-    word[3] = 'e';
-    /* word[4] = '\0';  // null */
+    // the easy way
+    char *word = "dave";
     printf("%s\n", word);
 }
