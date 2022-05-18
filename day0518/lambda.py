f882194001ef9719dd2b02cccb94a7f03bd988df
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-05-18 14:32:02 -0500

    Scripted auto-commit on change (2022-05-18 14:32:02) by gitwatch.sh

--- day0518/lambda.py
+++ day0518/lambda.py
@@ -12,7 +12,7 @@ print(data)
 #     return t[1]
 
 newdata = [("Dave", 1), ("Rachel", 1), ("Evan", 6), ("Lizzzzz", 12)]
-newdata.sort(key=lambda t: (t[1], t[0][3]))
+newdata.sort(key=lambda t: (t[1], t[0][2]))
 
 #newdata.sort(key=getSecondValue)
 print(newdata)
