6f0ef12c102209f1dae8343f07113a04f3888302
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-05-18 14:47:29 -0500

    Scripted auto-commit on change (2022-05-18 14:47:29) by gitwatch.sh

--- day0518/LambdaThing3.java
+++ day0518/LambdaThing3.java
@@ -2,7 +2,7 @@ interface FunThing {
     public int f(int x, int y);
 }
 
-public class LambdaThing2 {
+public class LambdaThing3 {
     public static void evaluate(FunThing ftobj,
                                 int x, int y) {
         System.out.println(ftobj.f(x,y));
@@ -10,13 +10,6 @@ public class LambdaThing2 {
 
     public static void main(String[] args) {
 
-        // FunThing adthing = new FunThing() {
-        //         public int f(int x, int y) {
-        //             return x + y;
-        //         }
-        //     };
-
-        // FunThing adthing = (x, y) -> x + y;
         evaluate((x, y) -> x + y, 3, 5);
     }
 }
