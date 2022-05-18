d801150f06acaf5dad6ce6fa86afb0d1e95b26c2
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-05-18 14:52:44 -0500

    Scripted auto-commit on change (2022-05-18 14:52:44) by gitwatch.sh

---
+++ day0518/LambdaThing4.java
@@ -0,0 +1,15 @@
+interface FunThing {
+    public int f(int x, int y);
+}
+
+public class LambdaThing3 {
+    public static void evaluate(FunThing ftobj,
+                                int x, int y) {
+        System.out.println(ftobj.f(x,y));
+    }
+
+    public static void main(String[] args) {
+
+        evaluate((x, y) -> x + y, 3, 5);
+    }
+}
