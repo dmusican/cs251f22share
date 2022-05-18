21e19a6e3b75ae6d0f237c6c5f5b3f3e68ccd080
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-05-18 14:53:55 -0500

    Scripted auto-commit on change (2022-05-18 14:53:55) by gitwatch.sh

--- day0518/LambdaThing4.java
+++ day0518/LambdaThing4.java
@@ -1,6 +1,6 @@
 import java.util.function.*;
 
-public class LambdaThing3 {
+public class LambdaThing4 {
     public static void evaluate(BiFunction<Integer, Integer, Integer> ftobj,
                                 int x, int y) {
         System.out.println(ftobj.apply(x,y));
